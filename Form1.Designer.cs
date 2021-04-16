8e8373f01e4cfdcddc9de874229c2cd7f5906319
SiddhaliGandhi <82670122+SiddhaliGandhi@users.noreply.github.com>
2021-04-17 00:21:26 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace ThinkBridge_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelProductID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelDiscription = new System.Windows.Forms.Label();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.textBoxProductID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxDiscription = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelProductID
+            // 
+            this.labelProductID.AutoSize = true;
+            this.labelProductID.Location = new System.Drawing.Point(43, 107);
+            this.labelProductID.Name = "labelProductID";
+            this.labelProductID.Size = new System.Drawing.Size(74, 17);
+            this.labelProductID.TabIndex = 0;
+            this.labelProductID.Text = "Product ID";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(43, 158);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(45, 17);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            // 
+            // labelDiscription
+            // 
+            this.labelDiscription.AutoSize = true;
+            this.labelDiscription.Location = new System.Drawing.Point(43, 225);
+            this.labelDiscription.Name = "labelDiscription";
+            this.labelDiscription.Size = new System.Drawing.Size(74, 17);
+            this.labelDiscription.TabIndex = 2;
+            this.labelDiscription.Text = "Discription";
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.AutoSize = true;
+            this.labelQuantity.Location = new System.Drawing.Point(43, 299);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(61, 17);
+            this.labelQuantity.TabIndex = 3;
+            this.labelQuantity.Text = "Quantity";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(43, 346);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(105, 17);
+            this.labelPrice.TabIndex = 4;
+            this.labelPrice.Text = "Price (per item)";
+            // 
+            // textBoxProductID
+            // 
+            this.textBoxProductID.Enabled = false;
+            this.textBoxProductID.Location = new System.Drawing.Point(158, 102);
+            this.textBoxProductID.Name = "textBoxProductID";
+            this.textBoxProductID.Size = new System.Drawing.Size(218, 22);
+            this.textBoxProductID.TabIndex = 5;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(158, 153);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(218, 22);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // textBoxDiscription
+            // 
+            this.textBoxDiscription.Location = new System.Drawing.Point(158, 204);
+            this.textBoxDiscription.Multiline = true;
+            this.textBoxDiscription.Name = "textBoxDiscription";
+            this.textBoxDiscription.Size = new System.Drawing.Size(218, 57);
+            this.textBoxDiscription.TabIndex = 7;
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(158, 294);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(218, 22);
+            this.textBoxQuantity.TabIndex = 8;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(158, 341);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(218, 22);
+            this.textBoxPrice.TabIndex = 9;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(46, 405);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(86, 28);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "ADD";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(169, 405);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(85, 28);
+            this.buttonModify.TabIndex = 11;
+            this.buttonModify.Text = "MODIFY";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(291, 405);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(85, 28);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(443, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(678, 331);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 542);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.textBoxDiscription);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxProductID);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelQuantity);
+            this.Controls.Add(this.labelDiscription);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelProductID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProductID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelDiscription;
+        private System.Windows.Forms.Label labelQuantity;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.TextBox textBoxProductID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxDiscription;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ThinkBridge_Assignment
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+        SqlCommand cmd;
+        SqlDataReader dr;
+        DataTable table = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if ((Int32.TryParse(textBoxQuantity.Text, out int value1)) && (Int32.TryParse(textBoxPrice.Text, out int value2)))
+            {
+                string query = "insert into ShopBridge values(" + Convert.ToInt32(textBoxProductID.Text) + ",'" + textBoxName.Text + "','" + textBoxDiscription.Text + "'," + Convert.ToInt32(textBoxQuantity.Text) + "," + Convert.ToInt32(textBoxPrice.Text) + ")";
+                cmd = new SqlCommand(query, cn);
+                try
+                {
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Record Added Successfully !!!");
+
+                    dataGridView1.DataSource = getProductDetails();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    cn.Close();
+                }
+            }
+            else
+            {
+                if ((!Int32.TryParse(textBoxQuantity.Text, out int i1)) && (!Int32.TryParse(textBoxQuantity.Text, out int i2)))
+                    MessageBox.Show("Quantity and Price must be integer values !!!");
+
+                else if (!Int32.TryParse(textBoxQuantity.Text, out int val1))
+                    MessageBox.Show("Quantity must be integer value !!!");
+
+                else if (!Int32.TryParse(textBoxPrice.Text, out int val2))
+                    MessageBox.Show("Price must be integer value !!!");
+            }
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if ((Int32.TryParse(textBoxQuantity.Text, out int value1)) && (Int32.TryParse(textBoxPrice.Text, out int value2)))
+                {
+                    string query = "update ShopBridge set name = '" + textBoxName.Text + "', discription = '" + textBoxDiscription.Text + "', quantity = " + Convert.ToInt32(textBoxQuantity.Text) + ", price = " + Convert.ToInt32(textBoxPrice.Text) + " where productID = " + Convert.ToInt32(textBoxProductID.Text);
+                    cmd = new SqlCommand(query, cn);
+                    try
+                    {
+                        cn.Open();
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                        MessageBox.Show("Record Modified Successfully !!!");
+
+                        dataGridView1.DataSource = getProductDetails();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        cn.Close();
+                    }
+                }
+                else
+                {
+                    if ((!Int32.TryParse(textBoxQuantity.Text, out int i1)) && (!Int32.TryParse(textBoxQuantity.Text, out int i2)))
+                        MessageBox.Show("Quantity and Price must be integer values !!!");
+
+                    else if (!Int32.TryParse(textBoxQuantity.Text, out int val1))
+                        MessageBox.Show("Quantity must be integer value !!!");
+
+                    else if (!Int32.TryParse(textBoxPrice.Text, out int val2))
+                        MessageBox.Show("Price must be integer value !!!");
+                }
+            }
+            else
+                MessageBox.Show("Before modifying the record, Please select the row from the table !!!");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if ((Int32.TryParse(textBoxQuantity.Text, out int value1)) && (Int32.TryParse(textBoxPrice.Text, out int value2)))
+                {
+                    string query = "delete from ShopBridge where productID = " + Convert.ToInt32(textBoxProductID.Text);
+                    cmd = new SqlCommand(query, cn);
+                    try
+                    {
+                        cn.Open();
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                        MessageBox.Show("Record Deleted Successfully !!!");
+
+                        dataGridView1.DataSource = getProductDetails();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        cn.Close();
+                    }
+                }
+                else
+                {
+                    if ((!Int32.TryParse(textBoxQuantity.Text, out int i1)) && (!Int32.TryParse(textBoxQuantity.Text, out int i2)))
+                        MessageBox.Show("Quantity and Price must be integer values !!!");
+
+                    else if (!Int32.TryParse(textBoxQuantity.Text, out int val1))
+                        MessageBox.Show("Quantity must be integer value !!!");
+
+                    else if (!Int32.TryParse(textBoxPrice.Text, out int val2))
+                        MessageBox.Show("Price must be integer value !!!");
+                }
+            }
+            else
+                MessageBox.Show("Before deleting the record, Please select the row from the table !!!");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+
+            textBoxProductID.Text = selectedRow.Cells[0].Value.ToString();
+            textBoxName.Text = selectedRow.Cells[1].Value.ToString();
+            textBoxDiscription.Text = selectedRow.Cells[2].Value.ToString();
+            textBoxQuantity.Text = selectedRow.Cells[3].Value.ToString();
+            textBoxPrice.Text = selectedRow.Cells[4].Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = getProductDetails();
+
+            string query = "select MAX(productid) from ShopBridge";
+            int cnt = 0;
+            cmd = new SqlCommand(query, cn);
+            try
+            {
+                cn.Open();
+                cnt = Convert.ToInt32(cmd.ExecuteScalar());
+                textBoxProductID.Text = (cnt + 1).ToString();
+                cn.Close();
+            }
+            catch (Exception)
+            {
+                cnt = 1;
+                textBoxProductID.Text = cnt.ToString();
+                cn.Close();
+            }
+        }
+
+        private DataTable getProductDetails()
+        {
+            table.Clear();
+            dataGridView1.Refresh();
+
+            cmd = new SqlCommand("select * from ShopBridge",cn);
+            try
+            {
+                cn.Open();
+                dr = cmd.ExecuteReader();
+                table.Load(dr);
+                dataGridView1.DataSource = table;
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cn.Close();
+            }
+            return table;
+        }
+    }
+}
